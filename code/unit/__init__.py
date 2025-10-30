4cdff68721b5a2a76b2b12574d3aa4f0ea7dd728
tfwuSUDA <154413690+tfwuSUDA@users.noreply.github.com>
2025-10-31 02:51:45 +0800

    Delete code directory

--- code/unit/__init__.py
+++
@@ -1,3 +0,0 @@
-from unit.mGraphSNN_code import mGraphSNN
-from unit.getFeatures import save_smiles_dicts, get_smiles_dicts, get_smiles_array, moltosvg_highlight
-from unit.ednn_utils import  Meter,MyDataset,EarlyStopping,collate_fn,IFM_DNN_4
